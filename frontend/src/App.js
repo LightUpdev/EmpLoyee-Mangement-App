4df430bd2a6876aa17703ae4bbf815a85ee33276
light_dev <dgr8steve95@gmail.com>
2024-10-03 10:14:42 +0100

    functionality to monitor device screen size

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,37 @@ import EmployeesManagement from "./pages/EmployeesManagement";
 import EmployeeDetailPage from "./components/EmployeeDetailPage";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      if (window.innerWidth < 768) {
+        setIsSmallScreen(true);
+      } else {
+        setIsSmallScreen(false);
+      }
+    };
+
+    // Check screen size when component mounts
+    checkScreenSize();
+
+    // Add resize event listener
+    window.addEventListener("resize", checkScreenSize);
+
+    // Clean up the event listener on component unmount
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, []);
+
   return (
     <div className="App">
+      {isSmallScreen && (
+        <div style={warningStyle}>
+          <p>Warning: This project is best viewed on a larger screen!</p>
+        </div>
+      )}
       <ToastContainer />
       <Router>
         <Routes>
@@ -18,5 +45,18 @@ function App() {
     </div>
   );
 }
+
+const warningStyle = {
+  position: "fixed",
+  bottom: "20px",
+  left: "50%",
+  transform: "translateX(-50%)",
+  backgroundColor: "#f8d7da",
+  color: "#721c24",
+  padding: "10px",
+  borderRadius: "5px",
+  boxShadow: "0 2px 5px rgba(0, 0, 0, 0.2)",
+  zIndex: 1000,
+};
 
 export default App;
